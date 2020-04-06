dce0c42f5f4524581e6788f3230e424529c3ba09
kharkov-timur <8cheri8@gmail.com>
2020-04-06 21:19:31 +0300

    Slider ООП

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-slider1.start();
+var slider1 = sliderFactory.createNewSlider();
+var slider2 = sliderFactory.createNewSlider();
+
+slider1.start('itk-slider1');
+slider2.start('itk-slider2');
--- js/slider.js
+++ js/slider.js
@@ -1,47 +1,60 @@
-var slider1 = {
-  imagesUrl: [],
-  showPrevBtn: document.getElementById('show-prev-btn'),
-  showNextBtn: document.getElementById('show-next-btn'),
-  slideImg: document.getElementById('slide-img'),
-  currentImageIndex: 0,
+var sliderFactory = {
+  createNewSlider: function () {
+    var newSlider = {
+      imagesUrl: [],
+      currentImageIndex: 0,
 
-  start: function () {
-    var that = this;
+      showPrevBtn: null,
+      showNextBtn: null,
+      slideImg: null,
 
-    // subscribe to events
-    this.showPrevBtn.addEventListener('click', function (e) {
-      that.onShowPrevBtnClick(e);
-    });
+      start: function (elId) {
+        var that = this;
+        var elSelector = '#' + elId;
+        var el = document.querySelector(elSelector);
 
-    this.showNextBtn.addEventListener('click', function (e) {
-      that.onShowNextBtnClick(e);
-    });
+        this.showPrevBtn = el.querySelector('.show-prev-btn');
+        this.showNextBtn = el.querySelector('.show-next-btn');
+        this.slideImg = el.querySelector('.slide-img');
 
-    // create images array
-    this.imagesUrl.push('https://www.topcarmonaco.com/images/cars/rent-lamborghini-aventador-s-roadster-top-car-monaco-hire-lamborghini-aventador-monaco-renting.jpg');
-    this.imagesUrl.push('https://storage1b.censor.net/images/2/c/8/9/2c891aa2a3ec4a86723435b0d117f542/original.jpg');
-    this.imagesUrl.push('https://cdn.motor1.com/images/mgl/zZnP8/s3/2020-rimac-c-two.jpg');
-    this.imagesUrl.push('https://www.novine.rademetalac.edu.rs/images/slike/Zanimljivosti/maxresdefault.jpg');
+        // subscribe to events
+        this.showPrevBtn.addEventListener('click', function (e) {
+          that.onShowPrevBtnClick(e);
+        });
 
-    this.slideImg.src = this.imagesUrl[this.currentImageIndex];
-    this.showPrevBtn.disabled = true;
-  },
+        this.showNextBtn.addEventListener('click', function (e) {
+          that.onShowNextBtnClick(e);
+        });
 
-  onShowPrevBtnClick: function (e) {
-    this.currentImageIndex--;
-    this.slideImg.src = this.imagesUrl[this.currentImageIndex];
-    this.showNextBtn.disabled = false;
-    if (this.currentImageIndex === 0) {
-      this.showPrevBtn.disabled = true;
-    }
-  },
+        // create images array
+        this.imagesUrl.push('https://www.topcarmonaco.com/images/cars/rent-lamborghini-aventador-s-roadster-top-car-monaco-hire-lamborghini-aventador-monaco-renting.jpg');
+        this.imagesUrl.push('https://storage1b.censor.net/images/2/c/8/9/2c891aa2a3ec4a86723435b0d117f542/original.jpg');
+        this.imagesUrl.push('https://cdn.motor1.com/images/mgl/zZnP8/s3/2020-rimac-c-two.jpg');
+        this.imagesUrl.push('https://www.novine.rademetalac.edu.rs/images/slike/Zanimljivosti/maxresdefault.jpg');
 
-  onShowNextBtnClick: function (e) {
-    this.currentImageIndex++;
-    this.slideImg.src = this.imagesUrl[this.currentImageIndex];
-    this.showPrevBtn.disabled = false;
-    if (this.currentImageIndex === (this.imagesUrl.length - 1)) {
-      this.showNextBtn.disabled = true;
-    }
+        this.slideImg.src = this.imagesUrl[this.currentImageIndex];
+        this.showPrevBtn.disabled = true;
+      },
+
+      onShowPrevBtnClick: function (e) {
+        this.currentImageIndex--;
+        this.slideImg.src = this.imagesUrl[this.currentImageIndex];
+        this.showNextBtn.disabled = false;
+        if (this.currentImageIndex === 0) {
+          this.showPrevBtn.disabled = true;
+        }
+      },
+
+      onShowNextBtnClick: function (e) {
+        this.currentImageIndex++;
+        this.slideImg.src = this.imagesUrl[this.currentImageIndex];
+        this.showPrevBtn.disabled = false;
+        if (this.currentImageIndex === (this.imagesUrl.length - 1)) {
+          this.showNextBtn.disabled = true;
+        }
+      }
+    };
+
+    return newSlider;
   }
 };
